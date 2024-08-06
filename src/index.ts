21a7e13da60fddf0f4845af21afbeb0bd1b32a0b
kalangeyash <yashkalange127@gmail.com>
2024-08-06 22:49:59 +0530

    performed CRUD operations on psql

--- src/index.ts
+++ src/index.ts
@@ -56,9 +56,10 @@ async function inserData(username:string,email:string,password:string)
         await client.connect()
         const insertQuery = `
             INSERT INTO users (username,email,password)
-            VALUES('${username}','${email}','${password}');
+            VALUES($1,$2,$3);
         `;
-        const data = await  client.query(insertQuery);
+        const values = [username,email,password]
+        const data = await  client.query(insertQuery,values);
         console.log("Insertion 200 " + data);
     }
     catch(err)
@@ -71,4 +72,60 @@ async function inserData(username:string,email:string,password:string)
 
 }
 
-inserData('kamna','kamna@gmail.com','cats-are-love')
+// inserData('Shubham','shubham@gmail.com','kalange7007')
+
+async function getUser(email:string) {
+
+
+    try {
+        await client.connect()
+            
+        const query = `SELECT * FROM users WHERE email = $1`;  
+        const value = [email]; 
+        
+        const result  = await  client.query(query,value)
+        
+        if(result.rows.length > 0){
+            console.log("Email found : " , result.rows[0])
+            return result.rows[0];
+        }
+        else{
+            console.log("No user found of that email ")
+            return null
+        }
+    } catch (error) {
+        console.error('Error during the insertion:', error);
+    }
+
+    finally{ await client.end()}
+}
+
+// getUser('shubham@gmail.com')
+
+async function getAllUsers() {
+    await client.connect()
+    const query = `SELECT * FROM users`;
+
+    const result = await client.query(query);
+    console.log(result.rows)
+
+    await client.end()
+    
+}
+// getAllUsers()
+
+async function changeUsername(prevUsername:string, newUsername : string){
+    await client.connect();
+
+    // const getElement = await client.query(`SELECT * FROM users ;`)
+    const query = `
+        UPDATE users
+        SET username = $2
+        WHERE username = $1;
+    `
+    const value = [prevUsername,newUsername];
+    const result = await client.query(query,value);
+
+    console.log(result.rows)
+}
+changeUsername('abhay' ,'SELECT * FROM users')
